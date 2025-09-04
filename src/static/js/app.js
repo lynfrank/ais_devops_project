e09f4a83d1a393c6f2811857ed500faea99d6e69
Franklin devops <franklin@gmail.com>
2025-09-04 12:52:49 +0200

    last bonjour

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -106,7 +106,7 @@ function AddItemForm({ onNewItem }) {
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
-                        {submitting ? 'Adding...' : 'Good Morning'}
+                        {submitting ? 'Adding...' : 'Bonjour'}
                     </Button>
                 </InputGroup.Append>
             </InputGroup>
